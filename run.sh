846bfb8f0d6051100c9c75b49dbc053e8e50fc4b
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2013-01-23 10:04:31 +0200

    url generation

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-siege -f ./urls.txt -c 100 -r 5 -d1 -i
+siege -f ./urls.txt -c 100 -r 5 -d1 -i -v
+#php urlgenerator.php|xargs -n1 | xargs -I@ siege -c 2 -r 1 -d1 -i -v @
--- urlgenerator.php
+++ urlgenerator.php
@@ -1,2 +1,9 @@
 <?php
-echo "http://localhost/set.php?value=".md5(rand(1,100))."\n";
+$maxTry = 100;
+$f = fopen('./urls.txt','w');
+foreach(range(0,$maxTry) as $i)	{
+	$line = "http://localhost/set.php?value=".md5(rand(1,100))."\n";
+	echo $line;
+	fwrite($f,$line);
+}
+fclose($f);
